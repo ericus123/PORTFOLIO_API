30eece0ebe2c873c5c792894309012c64c44305a
amani <amaniericus@gmail.com>
2020-08-20 09:34:13 +0200

    changed validation

--- validation.js
+++ validation.js
@@ -1,27 +1,50 @@
-
-//VALIDATION 
-const Joi = require('@hapi/joi');
+//VALIDATION
+const Joi = require("@hapi/joi");
 
 //Register validation
-const registerValidation = data => {
- const schema = Joi.object({
+const registerValidation = (data) => {
+  const schema = Joi.object({
     name: Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-}); 
-
+    password: Joi.string().min(6).required(),
+    bio: Joi.string().min(15).required(),
+  });
 
   // VALIDATING THE DATA BEFORE SAVING A USER
- return  schema.validate(data);
+  return schema.validate(data);
 };
-//Register validation
-const loginValidation = data => {
-    const schema = Joi.object({
-       email: Joi.string().min(6).required().email(),
-       password: Joi.string().min(6).required()
-   }); 
-     // VALIDATING THE DATA BEFORE SAVING A USER
-    return  schema.validate(data);
-   };
+//Login validation
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+  // VALIDATING THE DATA BEFORE SAVING A USER
+  return schema.validate(data);
+};
+
+// create post validation
+
+const postValidation = (data) => {
+  const schema = Joi.object({
+    title: Joi.string().min(15).required(),
+    description: Joi.string().min(500).required(),
+    author: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
+};
+
+//Message validation
+const messageValidation = (data) => {
+  const schema = Joi.object({
+    names: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    subject: Joi.string().min(6).required(),
+    message: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
+};
 module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
+module.exports.postValidation = postValidation;
+module.exports.messageValidation = messageValidation;
