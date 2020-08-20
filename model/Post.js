070db3e8147867522dfee9bfb527abe835f51288
amani <amaniericus@gmail.com>
2020-08-20 09:28:28 +0200

    added post model

---
+++ model/Post.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  description: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 6,
+  },
+  author: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Posts", postSchema);
