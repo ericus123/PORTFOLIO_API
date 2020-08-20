cc9209e9a73607943de812022de2a6544cd0adcf
amani <amaniericus@gmail.com>
2020-08-20 09:39:22 +0200

    added bio and updating user info option

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,12 @@ const bcrypt = require("bcryptjs");
 const path = require("path");
 const { registerValidation, loginValidation } = require("../validation");
 router.post("/register", async (req, res) => {
-  //VALIDATE THE DATA BEFORE WE MAKE A USER
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
   //Check if a user is already in the database
   const emailExists = await User.findOne({ email: req.body.email });
   if (emailExists) return res.status(400).send("Email already exists");
+  //VALIDATE THE DATA BEFORE WE MAKE A USER
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   //Hash the passwords
   const salt = await bcrypt.genSalt(10);
@@ -22,12 +21,50 @@ router.post("/register", async (req, res) => {
     name: req.body.name,
     email: req.body.email,
     password: hashedPassword,
+    bio: req.body.bio,
   });
   try {
     const savedUser = await user.save();
     res.send(savedUser);
   } catch (err) {
     res.status(400).send(err);
+  }
+});
+
+//update user info
+
+router.put("/update/:id", async (req, res) => {
+  //Check if a  name is already in the database
+  const nameExists = await User.findOne({ name: req.body.name });
+  if (nameExists) return res.status(400).send("Name already exists");
+
+  //Check if a user email is already in the database
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send("Email already exists");
+
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+
+    if (req.body.name) {
+      user.name = req.body.name;
+    }
+    if (req.body.email) {
+      user.email = req.body.email;
+    }
+    if (req.body.password) {
+      //Hash the passwords
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
+    }
+    if (req.body.bio) {
+      user.bio = req.body.bio;
+    }
+    await user.save();
+    res.send(user);
+  } catch {
+    res.status(404);
+    res.send({ error: "Ooops!!!! User doesn't exist!" });
   }
 });
 
@@ -52,6 +89,6 @@ router.post("/login", async (req, res) => {
   );
   res.header("auth-token", token);
 
-  res.sendFile(path.resolve("views/index.html"));
+  res.send("Welcome! You are now logged in");
 });
 module.exports = router;
