5c0a43f525f5400df411ba6536593498931ebe57
amani <amaniericus@gmail.com>
2020-08-24 13:40:10 +0200

    changed port

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.use("/api/posts", postRoute);
 app.use("/api/messages", messageRoute);
 
 //Listen for requests
-
-app.listen(3000, (err) => {
+const port = process.env.PORT || 3000;
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
   } else {
