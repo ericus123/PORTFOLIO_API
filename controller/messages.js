1981034c472abfc5624832078e7b4763e40e6772
amani <amaniericus@gmail.com>
2020-08-26 12:15:26 +0200

    deleted src

--- controller/messages.js
+++ controller/messages.js
@@ -30,7 +30,7 @@ const deletemsgController = async (req, res) => {
     const message = await Message.findOne({ _id: req.params.id });
 
     await message.deleteOne();
-    res.send({ eror: "Message deleted successfully !" });
+    res.send({ error: "Message deleted successfully !" });
   } catch {
     res.status(404);
     res.send({ error: "Not found message with id=" + req.params.id });
--- controller/updateUser.js
+++ controller/updateUser.js
@@ -1,29 +1,36 @@
-const User = require("../model/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+import User from "../model/User";
+import bcrypt from "bcryptjs";
 const updateuserController = async (req, res, next) => {
-  const id = req.user._id;
-
+  const id = req.user.id;
   //Check if a user email is already in the database
-  // const emailExists = await User.findOne({ email: req.body.email });
-  // if (emailExists)
-  // return res.status(400).send({ error: "Email already exists" });
-  // const user = await User.findOne({ id });
-  // //Hash the passwords
-  // const salt = await bcrypt.genSalt(10);
-  // const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send("Email already exists");
 
   try {
-    const updateduser = await User.updateOne(
-      { id },
-      { $set: { username: req.body.username, email: req.body.email } }
-    );
+    const user = await User.findOne({ _id: id });
+    if (!user) return res.status(404).json({ error: "user not found" });
 
-    res.send(updateduser);
+    if (req.body.name) {
+      user.name = req.body.name;
+    }
+    if (req.body.email) {
+      user.email = req.body.email;
+    }
+    if (req.body.password) {
+      //Hash the passwords
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
+    }
+    if (req.body.bio) {
+      user.bio = req.body.bio;
+    }
+    await user.save();
+    res.send(user);
   } catch {
     res.status(404);
     res.send({
-      error: "Ooops!!!! User with id doesn't exist!",
+      error: "error retrieving user",
     });
   }
 };
