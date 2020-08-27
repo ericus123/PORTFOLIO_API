eae1de6bfe297695ab75ebe5e349e459f790bdf9
amani <amaniericus@gmail.com>
2020-08-27 11:49:26 +0200

    added features

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
   const token = req.header("auth-token");
-  if (!token) return res.status(401).send({ eror: "Acces Denied" });
+  if (!token) return res.status(401).send({ error: "Acces Denied" });
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
