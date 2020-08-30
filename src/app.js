1fb4ef36f13a3e846213e3156af0742d4c645cf0
amani <amaniericus@gmail.com>
2020-08-30 09:58:26 +0200

    fixing cors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,19 @@ import axios from "axios";
 //express app
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 dotenv.config();
 
