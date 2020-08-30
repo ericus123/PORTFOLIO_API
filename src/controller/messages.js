b6f7b0a4266f7e220cf47b3a30e54a0c5ea7fe58
amani <amaniericus@gmail.com>
2020-08-30 14:42:43 +0200

    modified msg controller

--- src/controller/messages.js
+++ src/controller/messages.js
@@ -10,7 +10,7 @@ const messagesController = async (req, res) => {
   });
   try {
     const savedMessage = await message.save();
-    res.send(savedMessage);
+    res.send({ ms: savedMessage, success: "Message sent!" });
   } catch (err) {
     res.status(400).send(err);
   }
