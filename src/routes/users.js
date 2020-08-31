714bec3e00b05b7a6a9c86ccc6f5ce1b24581f4b
amani <amaniericus@gmail.com>
2020-08-31 13:53:25 +0200

    including cors policy middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import verify from "../middleware/verifyToken";
+import corsPolicy from "../middleware/cors";
 const userRoute = new Router();
 
 import { updateUserValidation } from "../middleware/validation";
@@ -7,10 +8,16 @@ import updateuserController from "../controller/updateUser";
 import getusersController from "../controller/getusers";
 import getprofileController from "../controller/getprofile";
 //get a list of all users
-userRoute.get("/getusers", verify, getusersController);
+userRoute.get("/getusers", corsPolicy, verify, getusersController);
 //update user info route
-userRoute.put("/update", verify, updateUserValidation, updateuserController);
+userRoute.put(
+  "/update",
+  corsPolicy,
+  verify,
+  updateUserValidation,
+  updateuserController
+);
 
 //get profile info
-userRoute.get("/getprofile", verify, getprofileController);
+userRoute.get("/getprofile", corsPolicy, verify, getprofileController);
 export default userRoute;
