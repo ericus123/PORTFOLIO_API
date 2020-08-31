dade2b10012184d0967decfa59fb0ee49ff00eba
amani <amaniericus@gmail.com>
2020-08-31 16:28:08 +0200

    fixing heroku

--- src/controller/posts.js
+++ src/controller/posts.js
@@ -1,6 +1,5 @@
 import Post from "../model/Post";
 const createpostController = async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
   //Create a new Post
   const post = new Post({
     title: req.body.title,
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import verify from "../middleware/verifyToken";
-import corsPolicy from "../middleware/cors";
 const userRoute = new Router();
 
 import { updateUserValidation } from "../middleware/validation";
@@ -10,13 +9,7 @@ import getprofileController from "../controller/getprofile";
 //get a list of all users
 userRoute.get("/getusers", verify, getusersController);
 //update user info route
-userRoute.put(
-  "/update",
-  corsPolicy,
-  verify,
-  updateUserValidation,
-  updateuserController
-);
+userRoute.put("/update", verify, updateUserValidation, updateuserController);
 
 //get profile info
 userRoute.get("/getprofile", verify, getprofileController);
