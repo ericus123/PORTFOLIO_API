5def5e126e2216bc12f33112d909b210ff9ee912
amani <amaniericus@gmail.com>
2020-09-02 13:28:14 +0200

    changed cors headers

--- src/controller/getprofile.js
+++ src/controller/getprofile.js
@@ -1,10 +1,7 @@
 import User from "../model/User";
 const getprofileController = async (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+  res.header("Access-Control-Allow-Headers", "*");
   next();
   const id = req.user.id;
   try {
