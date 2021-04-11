2da8ab4b966ce79e7dcdea01ad5e7785c683973e
ericus123 <amaniericus@gmail.com>
2021-04-11 14:55:48 +0200

    feat(subscribers):manage subscribers

--- src/controller/subscriptions/newsLetter.js
+++ src/controller/subscriptions/newsLetter.js
@@ -6,18 +6,18 @@ class NewsLetterController {
       const { email } = req.body;
       const subscribed = await NewsLetter.findOne({ email: email });
       if (subscribed) {
-        res.status(400).json({ error: "You've already subscribed" });
+        return res.status(400).json({ error: "You've already subscribed" });
       }
       const subscriber = new NewsLetter({
         email: email,
       });
       await subscriber.save();
-      res.status(201).json({
-        msg: "Subscribed",
+      return res.status(201).json({
+        msg: "Subscribed successfuly",
         subscriber: { email: email, createdAt: Date.now },
       });
     } catch (error) {
-      res.status(400).json({ error: "Something went wrong" });
+      return res.status(400).json({ error: "Something went wrong" });
     }
   }
   static async unsubscribe(req, res) {
@@ -25,14 +25,14 @@ class NewsLetterController {
       const { email } = req.params;
       const subscribed = await NewsLetter.findOne({ email: email });
       if (!subscribed) {
-        res.status(400).json({ error: "You've not subscribed" });
+        return res.status(400).json({ error: "You've not subscribed" });
       }
       const unsubscribe = await NewsLetter.deleteOne(subscribed);
-      res
+      return res
         .status(201)
         .json({ msg: "Unsubscribed successfuly", unsubscriber: subscribed });
     } catch (error) {
-      res.status(400).json({ error: "Something went wrong, try again" });
+      return res.status(400).json({ error: "Something went wrong, try again" });
     }
   }
 }
