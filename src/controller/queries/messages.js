5be64fb225e7409234953812198718dff8e15524
AMANI <amaniericus@gmail.com>
2022-01-26 22:00:53 +0200

    fix(messages):fix messages management

--- src/controller/queries/messages.js
+++ src/controller/queries/messages.js
@@ -1,37 +1,47 @@
 import Message from "../../model/Message";
 import dotenv from "dotenv";
+import { sendEmail, setEmail } from "../../helpers/emails";
+import { contactMessage } from "../../helpers/emails/templates";
 dotenv.config();
 const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-
 class messagesController {
-  static async sendMessage(req, res){
-    const message = new Message({
-      name: req.body.name,
-      email: req.body.email,
-      message: req.body.message,
-    });
+  static async sendMessage(req, res) {
     try {
-      const savedMessage = await message.save();
-      res
-        .status(200)
-        .json({ msg: savedMessage, success: "Thanks for contacting us!" });
+      const { name, message, email } = req.body;
+      const new_message = new Message({
+        name,
+        email,
+        message,
+      });
+      const savedMessage = await new_message.save();
+      await sendEmail(
+        setEmail(
+          process.env.EMAIL,
+          process.env.WEB_EMAIL,
+          "New Message",
+          contactMessage({ name, message, email })
+        )
+      );
+      res.status(200).json({ success: "Thanks for contacting us!" });
     } catch (err) {
       res.status(400).json({ error: err });
     }
-  };
-  static async getMessages (req, res){
+  }
+  static async getMessages(req, res) {
     try {
-      const messages =  await Message.find();
-      return res.status(200).json({msg:"Messages fetched successfuly", messages})
+      const messages = await Message.find();
+      return res
+        .status(200)
+        .json({ msg: "Messages fetched successfuly", messages });
     } catch (err) {
       return res.status(400).json({ error: err });
     }
-  };
-  
-  static async deleteMessage(req, res){
+  }
+
+  static async deleteMessage(req, res) {
     try {
       const message = await Message.findOne({ _id: req.params.id });
       await message.deleteOne();
@@ -40,9 +50,7 @@ class messagesController {
       res.status(404);
       res.send({ error: "Message with id=" + req.params.id + "is not found " });
     }
-  };
-  
+  }
 }
-
 
 export default messagesController;
