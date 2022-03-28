0bd0e657044f64f6283d32fe8d79ef90b1d0e9eb
AMANI Eric <amaniericus@gmail.com>
2022-03-28 11:39:48 +0200

    fix(contact): add email check on contact send

--- src/middleware/subscriptions/newsLetter.js
+++ src/middleware/subscriptions/newsLetter.js
@@ -1,7 +1,7 @@
 import validate from "deep-email-validator";
 class NewsLetterMiddleware {
   static async checkEmail(req, res, next) {
-    const email = req.body.email ? req.body.email : req.params.email;
+    const email = req.body.email || req.params.email;
     const { valid } = await validate(email);
     if (!valid) {
       return res.status(400).json({ error: "Email is invalid", email: email });
